4ad62a3129ad0636453d9b935f1f5aa1f3dcb2fd
David <david.aydin6@gmail.com>
2023-11-08 21:31:12 -0500

    Configured console and file logging with Serilog

--- Program.cs
+++ Program.cs
@@ -1,10 +1,19 @@
 using JokerService;
+using Serilog;
+
+var myDocuments = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
+Log.Logger = new LoggerConfiguration()
+    .WriteTo.Console()
+    // .WriteTo.File("log-.txt", rollingInterval: RollingInterval.Minute)
+    .WriteTo.File(Path.Combine(myDocuments, "Logs", "jokerservicelog.txt"))
+    .CreateLogger();
 
 IHost host = Host.CreateDefaultBuilder(args)
     .ConfigureServices(services =>
     {
         services.AddHostedService<Worker>();
     })
+    .UseSerilog()
     .Build();
 
 host.Run();
--- Worker.cs
+++ Worker.cs
@@ -17,4 +17,14 @@ public class Worker : BackgroundService
             await Task.Delay(1000, stoppingToken);
         }
     }
+    public override Task StartAsync(CancellationToken cancellationToken)
+    {
+        _logger.LogInformation("BackgroundService starting up...");
+        return base.StartAsync(cancellationToken);
+    }
+    public override Task StopAsync(CancellationToken cancellationToken)
+    {
+        _logger.LogInformation("BackgroundService stopping!");
+        return base.StopAsync(cancellationToken);
+    }
 }
