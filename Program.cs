f7db081f605cc3dc6005853b567bd3e9b5c74aad
David <david.aydin6@gmail.com>
2023-11-08 21:00:11 -0500

    dotnet new worker dotnet new gitignore

---
+++ Program.cs
@@ -0,0 +1,10 @@
+using JokerService;
+
+IHost host = Host.CreateDefaultBuilder(args)
+    .ConfigureServices(services =>
+    {
+        services.AddHostedService<Worker>();
+    })
+    .Build();
+
+host.Run();
