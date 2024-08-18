d8e8b85cfe335f460d31ec306aa22b6b2895dcd1
daydin14 <david.aydin6@gmail.com>
2024-08-17 23:29:38 -0400

    FileSystemSettings

---
+++ Settings/FileSystemSettings.cs
@@ -0,0 +1,16 @@
+﻿namespace JokerService.Settings
+{
+    public class FileSystemSettings
+    {
+        public required string RootDirectory { get; set; }
+        public required string JokesDirectory { get; set; }
+        public string? RemoteJokesDirectory { get; set; }
+        public bool ProcessRemoteJokes
+        {
+            get
+            {
+                return !string.IsNullOrWhiteSpace(RemoteJokesDirectory);
+            }
+        }
+    }
+}
